0e08104dfa2627446024e0865ba23d4269879f3d
vidurathegeek <viduradtej@gmail.com>
2020-04-11 18:38:15 +0530

    added GUI dropdown

---
+++ listviewGui.py
@@ -0,0 +1,59 @@
+from tkinter import *
+import tkinter as ttk
+#from ttk import *
+
+import sys
+import glob
+import serial
+
+root = Tk()
+root.title("Tk dropdown example")
+
+# Add a grid
+mainframe = Frame(root)
+mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
+mainframe.columnconfigure(0, weight = 1)
+mainframe.rowconfigure(0, weight = 1)
+mainframe.pack(pady = 100, padx = 100)
+
+# Create a Tkinter variable
+tkvar = StringVar(root)
+
+#getting serial ports
+def serial_ports():
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+# Dictionary with options
+tempchoi = serial_ports()
+choices = {i : tempchoi[i] for i in range(0, len(tempchoi) )}
+tkvar.set('Select COM') # set the default option
+
+popupMenu = OptionMenu(mainframe, tkvar, *choices)
+Label(mainframe, text="Choose a COM Port").grid(row = 1, column = 1)
+popupMenu.grid(row = 2, column =1)
+
+# on change dropdown value
+def change_dropdown(*args):
+    print( tkvar.get() )
+
+# link function to change dropdown
+tkvar.trace('w', change_dropdown)
+
+root.mainloop()
