626b3dcc19ca5043f1af523969a1ba1401c93f1d
Kasun Nirmala <bgkasunnirmala@gmail.com>
2020-04-11 16:41:33 +0530

    final

--- coro.py
+++ coro.py
@@ -2,9 +2,11 @@ import serial
 from pynput.keyboard import Key, Controller
 ser = serial.Serial("COM30", 9600)
 
-keyboard = Controller()
+release =True
+
 while True:
-    # bytesToRead = ser.inWaiting()
+    bytesToRead = ser.inWaiting()
+    keyboard = Controller()
     cc=str(ser.readline())
     print(cc)
     ss=(cc[2:3])
@@ -13,35 +15,46 @@ while True:
         print(ss)
 
         if ss=='U':
-             keyboard.press(Key.up)
+            release =True
+            keyboard.press(Key.up)
             #  keyboard.release(Key.down)
             #  keyboard.release(Key.left)
             #  keyboard.release(Key.right)
             #  keyboard.release(Key.up)
         elif ss== 'D':
+            release =True
             keyboard.press(Key.down)
             # keyboard.release(Key.up)
             # keyboard.release(Key.left)
             # keyboard.release(Key.right)
             # keyboard.release(Key.down)
         elif ss=='L':
+            release =True
             keyboard.press(Key.left)
             # keyboard.release(Key.down)
             # keyboard.release(Key.up)
             # keyboard.release(Key.right)
             # keyboard.release(Key.left)
         elif ss=='R':
+            release =True
             keyboard.press(Key.right)
             # keyboard.release(Key.down)
             # keyboard.release(Key.left)
             # keyboard.release(Key.up)
         elif ss=='S':
-            keyboard.release(Key.down)
-            keyboard.release(Key.left)
-            keyboard.release(Key.up)
-            keyboard.release(Key.right)
-            
+            if release:
+                release=False
+                keyboard.release(Key.down)
+                keyboard.release(Key.left)
+                keyboard.release(Key.up)
+                keyboard.release(Key.right)
+                keyboard = Controller()
+            # keyboard.release(Key.down)
+            # keyboard.release(Key.left)
+            # keyboard.release(Key.up)
             # keyboard.release(Key.right)
+            
+    # keyboard.release(Key.right)
       
         
     
